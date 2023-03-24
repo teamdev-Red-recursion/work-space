56ecb206efcdbf11e3c0dfdb4133b05825fafe73
sagogoal <yuyagoal@gmail.com>
2023-03-25 08:27:21 +0900

    modify connection pool

--- backend/pg_pool.js
+++ backend/pg_pool.js
@@ -1,11 +1,13 @@
 const { Client } = require('pg')
+const connectionString = 'postgres://' + process.env.DB_USER + ':' + process.env.DB_PW + '@' + process.env.DB_HOST + ':' + process.env.PORT + '/' + process.env.DB_NAME;
 const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PW,
-    port: process.env.DB_PORT,
+    connectionString: connectionString,
+    //user: process.env.DB_USER,
+    //host: process.env.DB_HOST,
+    //database: process.env.DB_NAME,
+    //password: process.env.DB_PW,
+    //port: process.env.DB_PORT,
     max: 10
-})
+});
 
 module.exports = client;
