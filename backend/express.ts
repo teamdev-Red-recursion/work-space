03c8adac549a353b9d510c6cfb249b9b1f266a78
sagogoal <yuyagoal@gmail.com>
2023-03-25 07:13:24 +0900

    add post put delete api

--- backend/express.ts
+++ backend/express.ts
@@ -4,9 +4,8 @@ const client = require('./pg_pool');
 const app = express()
 const port = process.env.EXPRESS_PORT || 3000
 
-
+//GET(select) all articles
 app.get("/articles", (req, res) => {
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000') TODO Reactのサーバーからのみ許可するように後で修正
     const query = "SELECT * FROM test"
     client.connect()
     .then(() => client.query(query))
@@ -19,8 +18,43 @@ app.get("/articles", (req, res) => {
     .finally(() => client.end())
 })
 
+//POST(insert) a article
+app.post("/articles", (req, res) => {
+    const query = "INSERT INTO test (id, name, zip, address, birth, sex) VALUES (1, 'yuya', '111-1111', 'tokyo', '2023-03-25', true)"
+    client.connect()
+    .then(() => client.query(query))
+    .then(() => res.send("ok!"))
+    .catch(e => console.log(e))
+    .finally(() => client.end())
+})
 
+//PUT(update) a article
+app.put("/articles:id", (req, res) => {
+    //TODO bodyで具体的な修正対象を送る→処理み実装
+    console.log(req.body);
 
-app.listen(port, "18.176.52.15", () => {
+    const query = "UPDATE test SET (id, name) = (999, 'change') WHERE id = 1"
+    client.connect()
+    .then(() => client.query(query))
+    .then(results => {
+        res.send({
+        articles: results.rows
+        })
+    })
+    .catch(e => console.log(e))
+    .finally(() => client.end())
+})
+
+//DELETE(delete) a article
+app.delete("/articles:id", (req, res) => {
+    const query = "DELETE from test where id = 1"
+    client.connect()
+    .then(() => client.query(query))
+    .then(() => res.send("ok!"))
+    .catch(e => console.log(e))
+    .finally(() => client.end())
+})
+
+app.listen(port, () => {
   console.log(`listening on *:${port}`);
 })
