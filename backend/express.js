05fd74f1ecb5175efbeb90a3d7b123583aab2dd0
sagogoal <yuyagoal@gmail.com>
2023-03-25 16:38:40 +0900

    modify connection pool

--- backend/express.js
+++ backend/express.js
@@ -5,57 +5,63 @@ const port = process.env.EXPRESS_PORT || 3000
 
 //GET(select) all articles
 app.get("/articles", async function(req, res) {
-    const query = "SELECT * FROM test"
-    const client = await pool.connect()
+    const selectQuery = "SELECT * FROM test"
+    const selectClient = await pool.connect()
     try {
-        const res = await client.query(query)
-        console.log(res.rows[0])
+        const res = await selectClient.query(selectQuery)
     } catch (err) {
         console.log(err.stack)
     } finally {
-        client.release()
+        selectClient.release()
     }
     res.send({
         articles: res.rows
     })
-
 })
 
 //POST(insert) a article
-app.post("/articles", (req, res) => {
-    const query = "INSERT INTO test (id, name, zip, address, birth, sex) VALUES (1, 'yuya', '111-1111', 'tokyo', '2023-03-25', true)"
-    pool.connect()
-    .then(() => pool.query(query))
-    .then(() => res.send("ok!"))
-    .catch(e => console.log(e))
-    .finally(() => pool.end())
+app.post("/articles", async function(req, res) {
+    const insertQuery = "INSERT INTO test (id, name, zip, address, birth, sex) VALUES (1, 'yuya', '111-1111', 'tokyo', '2023-03-25', true)"
+    const insertClient = await pool.connect()
+    try {
+        const res = await insertClient.query(insertQuery)
+    } catch (err) {
+        console.log(err.stack)
+    } finally {
+        insertClient.release()
+    }
+    res.send("ok!")
 })
 
 //PUT(update) a article
-app.put("/articles:id", (req, res) => {
+app.put("/articles:id", async function(req, res) {
     //TODO bodyで具体的な修正対象を送る→処理み実装
     console.log(req.body);
 
-    const query = "UPDATE test SET (id, name) = (999, 'change') WHERE id = 1"
-    pool.connect()
-    .then(() => pool.query(query))
-    .then(results => {
-        res.send({
-        articles: results.rows
-        })
-    })
-    .catch(e => console.log(e))
-    .finally(() => pool.end())
+    const updateQuery = "UPDATE test SET (id, name) = (999, 'change') WHERE id = 1"
+    const updateClient = await pool.connect()
+    try {
+        const res = await updateClient.query(updateQuery)
+    } catch (err) {
+        console.log(err.stack)
+    } finally {
+        updateClient.release()
+    }
+    res.send("ok!")
 })
 
 //DELETE(delete) a article
-app.delete("/articles:id", (req, res) => {
-    const query = "DELETE from test where id = 1"
-    pool.connect()
-    .then(() => pool.query(query))
-    .then(() => res.send("ok!"))
-    .catch(e => console.log(e))
-    .finally(() => pool.end())
+app.delete("/articles:id", async function(req, res) {
+    const deleteQuery = "DELETE from test where id = 1"
+    const deleteClient = await pool.connect()
+    try {
+        const res = await deleteClient.query(deleteQuery)
+    } catch (err) {
+        console.log(err.stack)
+    } finally {
+        deleteClient.release()
+    }
+    res.send("ok!")
 })
 
 app.listen(port, () => {
